5849e4d607d59528cb659ae66703f46c66068f8d
Henrique <henrique.thsantos.ths@gmail.com>
2021-06-10 15:51:02 -0300

    Configurando Webpack Server

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,5 @@ const livro = new Livro("Dom Quixote", 108.8, 0.1);
 
 // console.log(livro.precoComDesconto());
 
-$("body").append(`<h1>${livro.nome}</h1>`);
+$("body").append(`<h1>Nome: ${livro.nome}</h1>`);
 $("body").append(`<h2>Preço: R$${livro.precoComDesconto()}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     mode: "production", //development
     entry: "./src/main.ts",
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 9000,
+        hot: true,
+    },
     output: {
         filename: "app.min.js",
         path: path.join(__dirname, "dist"),
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: "public" }],
+        }),
+    ],
     resolve: {
         extensions: [".ts", ".js"],
     },
